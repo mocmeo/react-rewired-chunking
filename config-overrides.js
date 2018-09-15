c80d014cf38b850a6bb7a7dbe6d6b3ebf0d761c0
mocmeo <lamnnse04185@fpt.edu.vn>
2018-09-15 15:12:06 +0700

    Add library uglifyjs for wired app

--- config-overrides.js
+++ config-overrides.js
@@ -4,21 +4,21 @@ const rewireLess = require('react-app-rewire-less');
 const rewireVendorSplitting = require('react-app-rewire-vendor-splitting');
 
 module.exports = function override(config, env) {
-    // config = rewireUglifyjs(config);
+    config = rewireUglifyjs(config);
     // change importing css to less
-    // config = injectBabelPlugin(['import', {libraryName: 'antd', style: true}], config);
+    config = injectBabelPlugin(['import', {libraryName: 'antd', style: true}], config);
 
     // split vendors to a separate chunk
     config = rewireVendorSplitting(config, env);
 
-    // config = rewireLess.withLoaderOptions({
-    //     modifyVars: {
-    //         '@primary-color': '#1890ff',                         // primary color for all components
-    //         '@link-color': '#039be9',                               // link color
-    //         '@success-color': '#52c41a',                         // success state color
-    //         '@warning-color': '#faad14',                         // warning state color
-    //         '@error-color': '#f5222d',                           // error state color
-    //     }
-    // })(config, env);
+    config = rewireLess.withLoaderOptions({
+        modifyVars: {
+            '@primary-color': '#1890ff',                         // primary color for all components
+            '@link-color': '#039be9',                               // link color
+            '@success-color': '#52c41a',                         // success state color
+            '@warning-color': '#faad14',                         // warning state color
+            '@error-color': '#f5222d',                           // error state color
+        }
+    })(config, env);
     return config;
 }
